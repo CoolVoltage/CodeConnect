5180be14bf486cf887ce6d804f05d18cae1d5a62
Karthik <vijaypcguy@yahoo.com>
2013-07-24 23:30:19 +0530

    added backend validations

--- LAS/DBsubmit.php
+++ LAS/DBsubmit.php
@@ -2,6 +2,12 @@
 $dbc = mysqli_connect('127.0.0.1', 'root', 'pass', 'CodeConnect')
 or die('<span id="message">Error connecting to MySQL server.</span>');
 $Nick = mysqli_real_escape_string($dbc, trim($_GET['Nick']));
+$query = "SELECT * FROM UserInfo WHERE Nick='$Nick'";
+$result = mysqli_query($dbc, $query)
+or die('<span id="message">Error querying database.</span>');  
+if(preg_match('/^.{6,15}/',$_GET['Nick']) && preg_match('/\w/',$_GET['Nick']) && preg_match('/^\w[\w\.\+\-]+[@]\w{2,}[.]\w{2,3}/',$_GET['Email']) && preg_match('/@(?!(spambot|mailinator))/',$_GET['Email']) && preg_match('/\w{6,15}/',$_GET['Pass'])&&$_GET['Pass']==$_GET['RePass']&&(!is_array(mysqli_fetch_row($result))))
+{
+$Nick = mysqli_real_escape_string($dbc, trim($_GET['Nick']));
 $Email = mysqli_real_escape_string($dbc, trim($_GET['Email']));
 $Pass = mysqli_real_escape_string($dbc, trim($_GET['Pass']));
 $query = "INSERT INTO UserInfo(Nick,Email,Password)" .
@@ -9,4 +15,8 @@ $query = "INSERT INTO UserInfo(Nick,Email,Password)" .
 $result = mysqli_query($dbc, $query)
 or die('<span id="message">Error querying database.</span>');
 echo $_GET['Nick'];
+}
+else {
+echo('error');
+}
 ?>
